4551c5af34542e30773a820c2da2a110bb8d2850
Tui-PA <arthurpa_@hotmail.com>
2025-09-06 16:58:58 -0300

    Add Models

---
+++ WebApp/Models/Maregrafos.cs
@@ -0,0 +1,15 @@
+namespace devops_project.Models;
+public class Maregrafos
+{
+    public string SiglaMaregrafo { get; set; }
+    public string NomeMaregrafo { get; set; }
+    public string Local { get; set; }
+    public string SiglaUF { get; set; }
+    public DateTime DataInicialOperacao { get; set; }
+    public string UrlRelatorio { get; set; }
+    public double Lat { get; set; }
+    public double Lon { get; set; }
+    public int? IdGLOSS { get; set; }
+    public int IdUHSLC { get; set; }
+    public int IdPSMSL { get; set; }
+}
