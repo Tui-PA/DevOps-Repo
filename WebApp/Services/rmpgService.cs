8d8a708c62a56dff14d412a97686cca0edbfed82
Tui-PA <arthurpa_@hotmail.com>
2025-09-06 16:59:45 -0300

    Add Service to connect with external API

---
+++ WebApp/Services/rmpgService.cs
@@ -0,0 +1,58 @@
+using devops_project.Helpers;
+using devops_project.Interfaces;
+using devops_project.Models;
+using System.Text.Json;
+
+namespace devops_project.Services
+{
+    public class rmpgService : IrmpgService
+    {
+        HttpClient _httpClient;
+        private readonly ILogger _logger;
+        private readonly string baseUrlApi = "https://servicodados.ibge.gov.br/api/v1/rmpg";
+        private IPeriodHelper _periodHelper;
+
+        public rmpgService(ILogger<rmpgService> logger, PeriodHelper periodHelper)
+        {
+            _logger = logger;
+            _httpClient = new HttpClient();
+            _periodHelper = periodHelper;
+        }
+        
+        public async Task<List<Maregrafos>?> GetMaregrafosAsync()
+        {
+            string rmpgEndpointUrl = "/maregrafos";
+            string uriString = string.Concat(baseUrlApi, rmpgEndpointUrl);
+            _logger.LogInformation($"Starting request to RMPG API: {uriString}");
+            var rmpgReponse = await _httpClient.GetAsync(uriString);
+            
+            if (rmpgReponse.IsSuccessStatusCode)
+            {
+                var content = await rmpgReponse.Content.ReadAsStringAsync();
+                var maregrafos = JsonSerializer.Deserialize<List<Maregrafos>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                _logger.LogInformation(content);
+                return maregrafos;
+            }
+
+            return null;
+        }
+        
+        public async Task<List<Meteorologia>?> GetMeteorologiaAsync(string maregrafo, MeteorologiaPeriod period)
+        {
+            string rmpgEndpointUrl = $"/meteorologia/{maregrafo}?momentoInicial={_periodHelper.GetInitialDate(period):yyyy-MM-dd-HH-mm}&momentoFinal={_periodHelper.GetFinalDate:yyyy-MM-dd-HH-mm}";
+            string uriString = string.Concat(baseUrlApi, rmpgEndpointUrl);
+            _logger.LogInformation($"Starting request to RMPG API: {uriString}");
+            var rmpgReponse = await _httpClient.GetAsync(uriString);
+            
+            if (rmpgReponse.IsSuccessStatusCode)
+            {
+                var content = await rmpgReponse.Content.ReadAsStringAsync();
+                var meteorologia = JsonSerializer.Deserialize<List<Meteorologia>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                _logger.LogInformation(content);
+                return meteorologia;
+            }
+
+            return null;
+        }
+    }
+}
